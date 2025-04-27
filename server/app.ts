227b53a7f56291e56949a7ffaf1803a766e5cc9d
john lester escarlan <jlescarlan11@gmail.com>
2025-04-28 02:52:07 +0800

    initial commit

---
+++ server/app.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
