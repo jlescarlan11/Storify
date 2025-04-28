dba1c103551a68f76040cf329e523b5c539ed7c5
john lester escarlan <jlescarlan11@gmail.com>
2025-04-28 14:58:35 +0800

    added session

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,42 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "./generated/prisma/client";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://your-client-domain.com", // Your front-end URL
+    credentials: true, // Allow cookies to be sent
+  })
+);
+
+app.options(
+  "*",
+  cors({ origin: "https://your-client-domain.com", credentials: true })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 1000 * 60 * 60 * 24,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
